f37f34faa6c509be6ae49d8ee23e044d0cd97217
EndashawF <endashawdabi@gmail.com>
2025-05-14 20:26:39 -0400

    deploy

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -40,11 +40,15 @@ function Banner() {
           <button className="banner_button play">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+        {newFunction()}
       </div>
       <div className="banner_fadeBottom" />
     </div>
   );
+
+  function newFunction() {
+    return <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>;
+  }
 }
 
 export default Banner;
